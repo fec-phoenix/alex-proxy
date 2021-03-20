b077ec5cca46f69b68cdb941ef26403940823a1b
GalexyN <alexneeeeewin@icloud.com>
2021-03-20 12:15:18 -0700

    renders photo component

--- server/server.js
+++ server/server.js
@@ -20,14 +20,26 @@ app.get('/details/:propertyId', (req, res) => {
 
 app.get('/:listingID/reviews', (req, res) => {
   axios.get(`http://localhost:3006/${req.params.listingID}/reviews`)
-    .then(details => res.send(details.data))
+    .then(reviews => res.send(reviews.data))
     .catch(err => res.send(err));
 });
 
 app.get('/:propertyId/averageReviewsRating', (req, res) => {
   axios.get(`http://localhost:3006/${req.params.propertyId}/averageReviewsRating/`)
-    .then(details => res.send(details.data))
+    .then(averageReviews => res.send(averageReviews.data))
     .catch(err => res.send(err));
 });
+
+app.get('/photos/:propertyID', (req, res) => {
+  axios.get(`http://localhost:3002/photos/${req.params.propertyID}`)
+    .then(photos => res.send(photos.data))
+    .catch(err => res.send(err));
+});
+
+app.get('/photos/:propertyID/primaryPhoto', (req, res) => {
+  axios.get(`http://localhost:3002/photos/${req.params.propertyID}/primaryPhoto`)
+    .then(primaryPhoto => res.send(primaryPhoto.data))
+    .catch(err => res.send(err))
+})
 
 app.listen(3000, () => 'successfully connected to the proxy server on port 3000');
